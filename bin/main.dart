3c05442a02692714d919e5cb39d34d3f5e02e555
ymdarake <t.sakamoto.ouen@gmail.com>
2020-01-04 17:42:20 +1300

    [init]

---
+++ bin/main.dart
@@ -0,0 +1,31 @@
+import 'package:prompter_ymdarake/prompter_ymdarake.dart';
+import 'dart:io';
+
+void main() {
+  final prompter = Prompter();
+  final choice = prompter.askBinary('Are you here to convert an image?');
+  if (!choice) {
+    exit(0);
+  }
+
+  final format = prompter.askMultiple('Select format:', buildFormatOptions());
+  final selectedFile =
+      prompter.askMultiple('Select an image to convert:', buildFileOptions());
+}
+
+List<Option> buildFormatOptions() {
+  return [
+    Option('Convert to jpeg', 'jpeg'),
+    Option('Convert to png', 'png'),
+  ];
+}
+
+List<Option> buildFileOptions() {
+  return Directory.current.listSync().where((entity) {
+    return FileSystemEntity.isFileSync(entity.path) &&
+        entity.path.contains(RegExp(r'\.(png|jpg|jpeg)'));
+  }).map((entity) {
+    final filename = entity.path.split(Platform.pathSeparator).last;
+    return Option(filename, entity);
+  }).toList();
+}
